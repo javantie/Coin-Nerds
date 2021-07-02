9c323f988add8ae558f4f0bf66d649bb172baed9
RD <81473475+nylesor28@users.noreply.github.com>
2021-07-02 16:46:52 -0400

    add comments and remove some of the hardcoded top 5 cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ var CryptoCompareAPIKey = "47c595746df319744dafc11abb6db295cfe1ca9e302bec40e6c5a
     //  console.log(data);
     // });
  
-var fetchCoinTwitterFollowers =function(coinId){
+var fetchCoinTwitterFollowers = function(coinId){
   var apiURL = `https://min-api.cryptocompare.com/data/social/coin/latest?coinId=${coinId}&apikey=${CryptoCompareAPIKey}`
   fetch(apiURL)
   .then(function (response) {
@@ -47,6 +47,14 @@ var fetchCoinTwitterFollowers =function(coinId){
   });
 }
 
+
+/***************************************************************************************************
+ *  Function : buildTopFiveSection(data)
+ *  Description: 
+ *        1. Receives data from endpoint: /min-api.cryptocompare.com/data/top/totaltoptiervolfull
+ *        2. Loop through the array 5 times to retrieve relevant data to build out the top 5 section
+ *        3. Display section on the page
+ **********************************************************************************************/
 var buildTopFiveSection = function (data) {
   console.log(data)
   for(var i=0; i<5; i++){
@@ -66,28 +74,37 @@ var buildTopFiveSection = function (data) {
         pTickerName.classList= "ticker-name font-semibold text-blue-600 text-center bg-purple-100 rounded-t-md"
         pTickerName.textContent = tickerName  + "/"+toSymbol ;
 
-        var cardTextDiv = document.createElement("div")
-            cardTextDiv.classList = "py-2 pl-4 font-light"
-        var pPrice = document.createElement("p").textContent = price;
-        var p24HPctChange = document.createElement("p").textContent = change24HourPct;
-        var p1HPctChange = document.createElement("p").textContent = changeHourPct;
-        var numTwitterFollowers = document.createElement("p").textContent= "Place holder"
-        var br = document.createElement("br")
+    var cardTextDiv = document.createElement("div")
+        cardTextDiv.classList = "py-2 pl-4 font-light"
+    var pPrice = document.createElement("p").textContent = price;
+    var p24HPctChange = document.createElement("p").textContent = change24HourPct;
+    var p1HPctChange = document.createElement("p").textContent = changeHourPct;
+    var numTwitterFollowers = document.createElement("p").textContent= "Place holder"
+    var br = document.createElement("br")
 
-        top5Div.append(pTickerName)
-        
-        cardTextDiv.append(pPrice)
-        cardTextDiv.append(br)
-        cardTextDiv.append(p24HPctChange)
-        cardTextDiv.append(br)
-        cardTextDiv.append(p1HPctChange)
-        cardTextDiv.append(br)
-        cardTextDiv.append(numTwitterFollowers)
-        top5Div.append(cardTextDiv)
-        top5ContainerEl.append(top5Div)
+    top5Div.append(pTickerName)
+    
+    cardTextDiv.append(pPrice)
+    cardTextDiv.append(br)
+    cardTextDiv.append(p24HPctChange)
+    cardTextDiv.append(br)
+    cardTextDiv.append(p1HPctChange)
+    cardTextDiv.append(br)
+    cardTextDiv.append(numTwitterFollowers)
+    top5Div.append(cardTextDiv)
+    top5ContainerEl.append(top5Div)
   }
   
 }
+
+/************************************************************************
+ * Function: fetchCryptoCompareTopList()
+ * Description: 
+ *      1. Fetch the top 10 Crypo Currency at the time the data is
+ *         requested.
+ *      2. Calls buildTopFiveSection passing the data retrieved from 
+ *         the api call
+ ***********************************************************************/
 
 var fetchCryptoCompareTopList= function() {
   var limit = 10;
@@ -103,9 +120,16 @@ var fetchCryptoCompareTopList= function() {
     });
 }
 
+
+/************************************************************************
+ * Function : loadPage()
+ * Description: Initial function called to initally load the page
+ ************************************************************************/
 var loadPage = function(){
   fetchCryptoCompareTopList();
 }
 
 
+
+//function called on page load
 loadPage();
